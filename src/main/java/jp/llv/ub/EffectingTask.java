6d3e33d388283b11a31619090146c0b4ebaf70c8
toyblocks <a@llv.jp>
2016-11-23 17:46:55 +0900

    Initial commit

---
+++ src/main/java/jp/llv/ub/EffectingTask.java
@@ -0,0 +1,93 @@
+/* 
+ * Copyright (C) 2016 toyblocks
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jp.llv.ub;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class EffectingTask extends BukkitRunnable {
+
+    private static int RANGE = 10;
+    private static long CHECK_PERIOD = 20L;
+    private static Material VISIBLE = Material.BARRIER;
+    private static final Map<Player, EffectingTask> TASKS = new HashMap();
+
+    private static UsefulBarrier instance;
+    private final Player player;
+
+    private EffectingTask(Player target) {
+        this.player = target;
+    }
+
+    @Override
+    public void run() {
+        if ((!this.player.isOnline())
+                || (this.player.getGameMode() != GameMode.SURVIVAL)
+                || (this.player.getInventory().getItemInMainHand() == null)
+                || (this.player.getInventory().getItemInMainHand().getType() != VISIBLE)) {
+            cancel(this.player);
+            return;
+        }
+
+        Set<Location> locations = new HashSet();
+        for (int x = -RANGE; x <= RANGE; x++) {
+            for (int y = -RANGE; y <= RANGE; y++) {
+                for (int z = -RANGE; z <= RANGE; z++) {
+                    Location l = this.player.getLocation().getBlock().getLocation().clone().add(x, y, z);
+                    if (l.getBlock().getType() == Material.BARRIER) {
+                        locations.add(l.add(0.5D, 0.5D, 0.5D));
+                    }
+                }
+            }
+        }
+
+        locations.stream().forEach((l) -> this.player.spawnParticle(Particle.BARRIER, l, 1));
+    }
+
+    public static void init(UsefulBarrier instance, int range, long checkPeriod, Material visible) {
+        EffectingTask.instance = instance;
+        RANGE = range;
+        CHECK_PERIOD = checkPeriod;
+        VISIBLE = visible;
+    }
+
+    public static void call(Player target) {
+        if (instance == null) {
+            throw new IllegalStateException("not initialized");
+        }
+        EffectingTask newTask = new EffectingTask(target);
+        TASKS.put(target, newTask);
+        newTask.runTaskTimer(instance, 0L, CHECK_PERIOD);
+    }
+
+    public static void cancel(Player target) {
+        EffectingTask task = (EffectingTask) TASKS.get(target);
+        if (task == null) {
+            return;
+        }
+        task.cancel();
+        TASKS.remove(target);
+    }
+}
---
+++ src/main/java/jp/llv/ub/UsefulBarrier.java
@@ -0,0 +1,163 @@
+/* 
+ * Copyright (C) 2016 toyblocks
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jp.llv.ub;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.logging.Level;
+import org.bukkit.Effect;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UsefulBarrier extends JavaPlugin implements Listener {
+
+    private static final Set<ItemDrop> ITEM_DROPS = new HashSet();
+
+    private static Material BREAKER;
+    private static Material VISIBLE;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        FileConfiguration conf = getConfig();
+        try {
+            BREAKER = Material.valueOf(conf.getString("break_tool").toUpperCase());
+            VISIBLE = Material.valueOf(conf.getString("visible_tool").toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            getLogger().warning("No such material. Now using default tool");
+            BREAKER = Material.BARRIER;
+            VISIBLE = Material.BARRIER;
+        }
+        EffectingTask.init(this, conf.getInt("check_range"), conf.getLong("check_freq"), VISIBLE);
+        ConfigurationSection drops = conf.getConfigurationSection("drops");
+        for (String name : drops.getKeys(false)) {
+            Material m;
+            try {
+                m = Material.valueOf(name.toUpperCase());
+            } catch (IllegalArgumentException ex) {
+                getLogger().log(Level.WARNING, "No such material: {0}", name);
+                continue;
+            }
+            ITEM_DROPS.add(new ItemDrop(m, drops.getDouble(name)));
+        }
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private void checkItemInHand(Player player) {
+        if ((player.isOnline())
+                && (player.getGameMode() == GameMode.SURVIVAL)
+                && (player.getInventory().getItemInMainHand() != null) && (player.getInventory().getItemInMainHand().getType() == Material.BARRIER)) {
+            EffectingTask.call(player);
+        } else {
+            EffectingTask.cancel(player);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemEvent(PlayerItemHeldEvent event) {
+        checkItemInHand(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemEvent(PlayerJoinEvent event) {
+        checkItemInHand(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemEvent(PlayerQuitEvent event) {
+        checkItemInHand(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemEvent(PlayerDropItemEvent event) {
+        checkItemInHand(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemEvent(PlayerGameModeChangeEvent event) {
+        checkItemInHand(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemEvent(InventoryClickEvent event) {
+        if ((event.getWhoClicked() instanceof Player)) {
+            checkItemInHand((Player) event.getWhoClicked());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemEvent(PlayerPickupItemEvent event) {
+        checkItemInHand(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPlayerInteractBarrier(PlayerInteractEvent event) {
+        if ((event.getAction() != Action.LEFT_CLICK_BLOCK) || (event.getPlayer().getInventory().getItemInMainHand().getType() != BREAKER) || (event.getPlayer().getGameMode() != GameMode.SURVIVAL) || (event.getClickedBlock().getType() != Material.BARRIER)) {
+            return;
+        }
+        BlockBreakEvent newEvent = new BlockBreakEvent(event.getClickedBlock(), event.getPlayer());
+        getServer().getPluginManager().callEvent(newEvent);
+        if (newEvent.isCancelled()) {
+            return;
+        }
+        Location l = event.getClickedBlock().getLocation();
+        event.getClickedBlock().setType(Material.AIR);
+        l.getWorld().playEffect(l, Effect.STEP_SOUND, Material.GLASS);
+        ITEM_DROPS.stream().forEach(id -> event.getClickedBlock().getWorld().dropItem(l, id.getDrops()));
+    }
+
+    private static class ItemDrop {
+
+        private static final Random RANDOM = new Random();
+        private final Material material;
+        private final double percentage;
+
+        public ItemDrop(Material material, double percentage) {
+            this.material = material;
+            this.percentage = percentage;
+        }
+
+        public ItemStack getDrops() {
+            int amount = (int) (this.percentage / 100.0D);
+            double chance = this.percentage / 100.0D - amount;
+            if (chance >= RANDOM.nextDouble()) {
+                amount++;
+            }
+            return new ItemStack(this.material, amount);
+        }
+    }
+}
